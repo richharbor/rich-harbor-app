de268e6c72def7966b0d91b33bdf5c9c1b2c416e
prabhat <prabhat@rhinontech.com>
2025-08-07 14:11:06 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex w-full h-screen justify-center items-center text-3xl font-bold'>
+      Welcome to Rich Harbor
+    </div>
+  )
+}
+
+export default page
