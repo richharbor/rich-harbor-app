05c650ad0abedc05443682695b048126387c9409
Varun Mathiyalagan <140169607+Varun2773@users.noreply.github.com>
2025-10-24 18:38:37 +0530

    pdf bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        // Match all PDF files in /public
+        source: "/:file*.pdf",
+        headers: [
+          {
+            key: "Content-Disposition",
+            value: "attachment",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
